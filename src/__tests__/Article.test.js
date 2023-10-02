8d762ca94d03f633bab86fcda028b0e04e5d468a
LingZhouChen <lingzhouc@gmail.com>
2023-10-02 16:50:51 -0400

    complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,12 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  // give sense of what the DOM looks like
+  screen.debug();
+  
+  const element = screen.queryByText("please pass this test");
+  screen.debug(element);
+  expect(element).toBeInTheDocument();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
